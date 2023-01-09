5bcb6fef6cc6b86c34e97faeb28b4d2d3ff7099d
b-rajshekar <brajashekhar19@gmail.com>
2023-01-09 21:27:28 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css'
+import * as React from "react";
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
-export default function App() {
+function App() {
   return (
-    <main>
-      Hare Krishna
-    </main>
-  )
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
+
+export default App;
---
+++ src/Content.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const Content = () => {
+
+    return (
+        <main>
+            <h1>Content</h1>
+        </main>
+    )
+}
+export default Content
